4d61748b0d5610e48d6374043f6b7b90ead0cc96
Nathan Wilcox <nejucomo@gmail.com>
2014-06-26 08:23:12 -0700

    Start specifying argument parsing and logging behavior.

--- leastbot/tests/test_main.py
+++ leastbot/tests/test_main.py
@@ -1,12 +1,14 @@
+import sys
+import logging
 from types import FunctionType
 
 from mock import call
 
-from leastbot.main import main
+from leastbot.main import main, parse_args, LogFormat, DateFormat
 from leastbot.tests.mockutil import MockingTestCase, ArgIsType
 
 
-class mainTests (MockingTestCase):
+class main_Tests (MockingTestCase):
     def test_main_twisted_setup(self):
         m_reactor = self.make_mock()
         m_WebServer = self.patch('leastbot.webserver.WebServer')
@@ -24,3 +26,26 @@ class mainTests (MockingTestCase):
         self.assert_calls_equal(
             m_reactor,
             [call.run()])
+
+
+class parse_args_Tests (MockingTestCase):
+    def test_help(self):
+        self.assertRaises(SystemExit, parse_args, ['--help'])
+
+    def test_no_args(self):
+        m_basicConfig = self.patch('logging.basicConfig')
+
+        parse_args([])
+
+        self.assert_calls_equal(
+            m_basicConfig,
+            [call(stream=sys.stdout, format=LogFormat, datefmt=DateFormat, level=logging.INFO)])
+
+    def test_log_level(self):
+        m_basicConfig = self.patch('logging.basicConfig')
+
+        parse_args(['--log-level', 'DEBUG'])
+
+        self.assert_calls_equal(
+            m_basicConfig,
+            [call(stream=sys.stdout, format=LogFormat, datefmt=DateFormat, level=logging.DEBUG)])
