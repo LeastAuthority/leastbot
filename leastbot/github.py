9a4453a872d4d38eccb713aedafb3cc95257e881
Nathan Wilcox <nejucomo@gmail.com>
2014-06-25 21:23:54 -0700

    Implement the SignatureVerifier with constant time taken from rdist.

--- leastbot/github.py
+++ leastbot/github.py
@@ -19,6 +19,23 @@ class SignatureVerifier (object):
     def __init__(self, sharedsecret):
         self._sharedsecret = sharedsecret
 
+    def __call__(self, allegedsig, message):
+        expectedsig = self._calculate_hmacsha1(message)
+        return constant_time_compare(allegedsig, expectedsig)
+
     def _calculate_hmacsha1(self, body):
         m = hmac.HMAC(key=self._sharedsecret, msg=body, digestmod=hashlib.sha1)
         return m.hexdigest()
+
+
+def constant_time_compare(a, b):
+    # Use Nate Lawson's constant time compare:
+    # http://rdist.root.org/2010/01/07/timing-independent-array-comparison/
+
+    if len(a) != len(b):
+        return False
+
+    result = 0
+    for (x, y) in zip(a, b):
+        result |= ord(x) ^ ord(y)
+    return result == 0
