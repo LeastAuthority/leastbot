0343bc0460821efbbfacc8a5b29a0e4132fa5758
Nathan Wilcox <nejucomo@gmail.com>
2014-06-30 10:40:13 -0700

    Update the webserver constructor/listen spec to follow new style (this is to be analogous / consistent with irc.Client).

--- leastbot/tests/test_webserver.py
+++ leastbot/tests/test_webserver.py
@@ -9,12 +9,13 @@ class WebserverTests (MockingTestCase):
         MockingTestCase.setUp(self)
 
         self.f_secret = 'fake secret'
+        self.f_port = 1234
         self.m_reactor = self.make_mock()
         self.m_handle_event = self.make_mock()
         self.m_WebhookResource = self.patch('leastbot.github.WebhookResource')
         self.m_Site = self.patch('twisted.web.server.Site')
 
-        self.s = webserver.WebServer(self.m_reactor, self.f_secret, self.m_handle_event)
+        self.s = webserver.WebServer(self.m_reactor, self.f_port, self.f_secret, self.m_handle_event)
 
     def test___init__(self):
         self.assert_calls_equal(self.m_reactor, [])
@@ -23,6 +24,6 @@ class WebserverTests (MockingTestCase):
         self.assert_calls_equal(self.m_Site, [call(self.m_WebhookResource.return_value)])
 
     def test_listen(self):
-        self.s.listen(1234)
+        self.s.listen()
 
-        self.assert_calls_equal(self.m_reactor, [call.listenTCP(1234, self.m_Site.return_value)])
+        self.assert_calls_equal(self.m_reactor, [call.listenTCP(self.f_port, self.m_Site.return_value)])
--- leastbot/webserver.py
+++ leastbot/webserver.py
@@ -4,9 +4,10 @@ from leastbot import github
 
 
 class WebServer (object):
-    def __init__(self, reactor, secret, handle_event):
+    def __init__(self, reactor, port, secret, handle_event):
         self._reactor = reactor
+        self._port = port
         self._site = server.Site(github.WebhookResource(secret, handle_event))
 
-    def listen(self, port):
-        self._reactor.listenTCP(port, self._site)
+    def listen(self):
+        self._reactor.listenTCP(self._port, self._site)
