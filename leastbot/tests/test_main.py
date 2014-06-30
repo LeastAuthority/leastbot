231456b9612517d3869a4abb604875979b7b8ba9
Nathan Wilcox <nejucomo@gmail.com>
2014-06-30 09:04:12 -0700

    Add spec for unexpected arguments to main triggering usage error.

--- leastbot/tests/test_main.py
+++ leastbot/tests/test_main.py
@@ -4,24 +4,30 @@ from types import FunctionType
 from mock import call, sentinel
 
 from leastbot.main import main, init_logging, parse_args, LogFormat, DateFormat
-from leastbot.tests.mockutil import MockingTestCase, ArgIsType
+from leastbot.tests.logutil import LogMockingTestCase
+from leastbot.tests.mockutil import ArgIsType
 
 
-class LoggingPatcherTestCase (MockingTestCase):
+class LogInitializationTestCase (LogMockingTestCase):
     def setUp(self):
-        MockingTestCase.setUp(self)
+        LogMockingTestCase.setUp(self)
 
         self.m_basicConfig = self.patch('logging.basicConfig')
         self.m_PythonLoggingObserver = self.patch('twisted.python.log.PythonLoggingObserver')
+        self.m_stdout = self.patch('sys.stdout')
+        self.m_stderr = self.patch('sys.stderr')
 
 
-class main_Tests (LoggingPatcherTestCase):
+class main_Tests (LogInitializationTestCase):
+    def setUp(self):
+        LogInitializationTestCase.setUp(self)
+
+        self.m_reactor = self.make_mock()
+        self.m_Client = self.patch('leastbot.irc.Client')
+        self.m_WebServer = self.patch('leastbot.webserver.WebServer')
+
     def test_main_no_args(self):
-        m_reactor = self.make_mock()
-        m_Client = self.patch('leastbot.irc.Client')
-        m_WebServer = self.patch('leastbot.webserver.WebServer')
-
-        main(args=[], reactor=m_reactor)
+        main(args=[], reactor=self.m_reactor)
 
         websecret = 'abc'
         webport = 8080
@@ -34,51 +40,59 @@ class main_Tests (LoggingPatcherTestCase):
 
 
         self.assert_calls_equal(
-            m_WebServer,
-            [call(m_reactor, websecret, ArgIsType(FunctionType)),
+            self.m_WebServer,
+            [call(self.m_reactor, websecret, ArgIsType(FunctionType)),
              call().listen(webport)])
 
         self.assert_calls_equal(
-            m_Client,
-            [call(m_reactor, irchost, ircport, nick, password, nickserv, channel),
+            self.m_Client,
+            [call(self.m_reactor, irchost, ircport, nick, password, nickserv, channel),
              call().connect()])
 
         self.assert_calls_equal(
-            m_reactor,
+            self.m_reactor,
             [call.run()])
 
+    def test_main_args_is_UsageError(self):
+        self.assertRaises(SystemExit, main, args=['foo'], reactor=self.m_reactor)
 
-class parse_args_Tests (LoggingPatcherTestCase):
-    def setUp(self):
-        LoggingPatcherTestCase.setUp(self)
+        # Assert no I/O subsystems were touched:
+        for m in [self.m_reactor, self.m_WebServer, self.m_Client]:
+            self.assert_calls_equal(m, [])
 
-        self.m_stdout = self.patch('sys.stdout')
-        self.m_stderr = self.patch('sys.stderr')
 
-    def assertNoOutputOrLogInit(self):
+
+class parse_args_Tests (LogInitializationTestCase):
+    def _assert_no_log_init(self):
+        self.assert_calls_equal(self.m_basicConfig, [])
+        self.assert_calls_equal(self.m_PythonLoggingObserver, [])
+
+    def _assert_no_output_or_log_init(self):
         self.assert_calls_equal(self.m_stdout, [])
         self.assert_calls_equal(self.m_stderr, [])
-        self.assert_calls_equal(self.m_basicConfig, [])
-        self.assert_calls_equal(self.m_PythonLoggingObserver, [])
-
-    def test_help(self):
-        self.assertRaises(SystemExit, parse_args, ['--help'])
-
-        self.assert_calls_equal(self.m_stdout, [call.write(ArgIsType(str))])
-        self.assert_calls_equal(self.m_stderr, [])
-        self.assert_calls_equal(self.m_basicConfig, [])
-        self.assert_calls_equal(self.m_PythonLoggingObserver, [])
 
     def test_no_args(self):
         parse_args([])
-        self.assertNoOutputOrLogInit()
+        self._assert_no_output_or_log_init()
 
     def test_log_level(self):
         parse_args(['--log-level', 'DEBUG'])
-        self.assertNoOutputOrLogInit()
+        self._assert_no_output_or_log_init()
+
+    def test_help(self):
+        self.assertRaises(SystemExit, parse_args, ['--help'])
+        self._assert_no_log_init()
+        self.failUnless(self.m_stdout.write.called)
+        self.failIf(self.m_stderr.write.called)
+
+    def test_unexpected_args(self):
+        self.assertRaises(SystemExit, parse_args, ['bananas!'])
+        self._assert_no_log_init()
+        self.failIf(self.m_stdout.write.called)
+        self.failUnless(self.m_stderr.write.called)
 
 
-class init_logging_Tests (LoggingPatcherTestCase):
+class init_logging_Tests (LogInitializationTestCase):
     def test_init_logging(self):
         init_logging(sentinel.A_LOG_LEVEL)
 
